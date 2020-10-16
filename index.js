71abc3275792e9d389c12822ca5e5b3cc74f4f3d
Siydulislam <siydulislam333@gmail.com>
2020-10-16 11:52:07 +0600

    cleanup the code

--- index.js
+++ index.js
@@ -17,121 +17,95 @@ app.use(fileUpload());
 
 const port = 5000;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send("Hello, how are you doing?")
 })
 
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const servicesCollection = client.db(process.env.DB_NAME).collection("services");
-  const reviewsCollection = client.db(process.env.DB_NAME).collection("reviews");
-  const ordersCollection = client.db(process.env.DB_NAME).collection("orders");
-  const adminCollection = client.db(process.env.DB_NAME).collection("admin");
-  console.log('database connected successfully');
+    const servicesCollection = client.db(process.env.DB_NAME).collection("services");
+    const reviewsCollection = client.db(process.env.DB_NAME).collection("reviews");
+    const ordersCollection = client.db(process.env.DB_NAME).collection("orders");
+    const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+    console.log('database connected successfully');
 
-  app.post('/addServices', (req, res) => {
-      const file = req.files.file;
-      const title = req.body.title;
-      const description = req.body.description;
-      const filePath = `${__dirname}/services/${file.name}`;
-      file.mv(filePath, err => {
-          if(err){
-              console.log(err);
-              res.status(500).send({meg:'Failed to upload Image'});
-          }
-          const newImg = fs.readFileSync(filePath);
-          const encImg = newImg.toString('base64');
+    app.post('/addServices', (req, res) => {
+        const file = req.files.file;
+        const title = req.body.title;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
 
-          const image = {
-            contentType: req.files.file.mimetype,
-            size: req.files.file.size,
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
             img: Buffer.from(encImg, 'base64')
-          };
+        };
 
-          servicesCollection.insertOne({title, description, image})
-          .then(result => {
-              fs.remove(filePath, error => {
-                  if(error) {
-                      console.log(error);
-                      res.status(500).send({meg:'Failed to upload Image'});
-                    }
-                  res.send(result.insertedCount > 0)
-              })
-          })
-      })
-  })
+        servicesCollection.insertOne({ title, description, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
 
-  app.get('/services', (req, res) => {
-      servicesCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-  })
+    app.get('/services', (req, res) => {
+        servicesCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
     app.post('/addReviews', (req, res) => {
-      const file = req.files.file;
-      const name = req.body.name;
-      const designation = req.body.designation;
-      const description = req.body.description;
-      const filePath = `${__dirname}/reviews/${file.name}`;
-      file.mv(filePath, err => {
-          if(err){
-              console.log(err);
-              res.status(500).send({meg:'Failed to upload Image'});
-          }
-          const newImg = fs.readFileSync(filePath);
-          const encImg = newImg.toString('base64');
+        const file = req.files.file;
+        const name = req.body.name;
+        const designation = req.body.designation;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
 
-          const image = {
-            contentType: req.files.file.mimetype,
-            size: req.files.file.size,
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
             img: Buffer.from(encImg, 'base64')
-          };
+        };
 
-          reviewsCollection.insertOne({name, designation, description, image})
-          .then(result => {
-              fs.remove(filePath, error => {
-                  if(error) {
-                      console.log(error);
-                      res.status(500).send({meg:'Failed to upload Image'});
-                    }
-                  res.send(result.insertedCount > 0)
-              })
-          })
-      })
-  })
+        reviewsCollection.insertOne({ name, designation, description, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
 
-  app.get('/reviews', (req, res) => {
-      reviewsCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-  })
+    app.get('/reviews', (req, res) => {
+        reviewsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-  app.post('/orders', (req, res) => {
-      const order = req.body;
-      ordersCollection.insertOne(order)
-      .then(result => {
-          res.send(result);
-      })
-  })
+    app.post('/orders', (req, res) => {
+        const order = req.body;
+        ordersCollection.insertOne(order)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
 
-  app.get('/ordersList', (req, res) => {
-      ordersCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-  })
+    app.get('/ordersList', (req, res) => {
+        ordersCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-  app.get('/servicesList', (req, res) => {
-      servicesCollection.find({email: req.query.email})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
-  })
+    app.get('/servicesList', (req, res) => {
+        servicesCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-   app.post('/isAdmin', (req, res) => {
+    app.post('/isAdmin', (req, res) => {
         const email = req.body.email;
         adminCollection.find({ email: email })
             .toArray((err, admins) => {
